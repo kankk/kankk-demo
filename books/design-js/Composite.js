a9dcda184c4c745a0758bc56db4495a0e9c7387b
KanKK <286454796@qq.com>
2017-11-28 23:15:26 +0800

    update
    book-JavaScript设计模式与开发实践-组合模式

---
+++ books/design-js/Composite.js
@@ -0,0 +1,27 @@
+var MacroCommand = function() {
+  return {
+    commandsList: [],
+    add: function(command) {
+      this.commandsList.push(command);
+    },
+    execute: function() {
+      for(var i = 0, command; command = this.commandsList[i++];) {
+        command.execute();
+      }
+    }
+  }
+};
+
+var openTvCommand = {
+  execute: function() {
+    console.log('打开电视');
+  },
+  add: function() {
+    throw new Error('叶子对象不能添加子节点');
+  }
+};
+
+var macroCommand = MacroCommand();
+
+macroCommand.add(openTvCommand);
+// openTvCommand.add(macroCommand); // Uncaught Error: 叶子对象不能添加子节点
