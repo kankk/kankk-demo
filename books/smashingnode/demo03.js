90544b322cf6ce7297db87a4beb3e743337cacf1
kankk <286454796@qq.com>
2018-03-06 16:29:41 +0800

    '添加nodejs文档demo代码'

--- books/smashingnode/demo03.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const files = fs.readdirSync(process.cwd());
-files.forEach((file) => {
-  if (/\.js/.test(files)) {
-    fs.watchFile(process.cwd() + '/' + file, () => {
-      console.log(' - ' + file + ' changed!');
-    });
-  }
-});
