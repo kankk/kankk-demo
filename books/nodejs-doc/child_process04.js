7d3b02a6e127f8d3e4a506cbb9cee4e834243214
KanKK <286454796@qq.com>
2018-03-13 23:53:30 +0800

    update

--- books/nodejs-doc/child_process04.js
+++ books/nodejs-doc/child_process04.js
@@ -34,3 +34,21 @@
 // <boolean>: 调用subprocess.disconnect()后会被设为false
 // subprocess.connected属性表明是否扔可以从一个子进程发送和接受信息
 // 当subprocess.connected为false时, 则不能在发送或接收的消息
+
+// subprocess.disconnect()
+// 关闭父进程与子进程之间的IPC通道, 一旦没有其他的连接使其保持活跃, 则允许子进程正常退出.
+// 调用该方法后, 父进程和子进程各自的subprocess.connected和process.connected属性都会设为false, 且进程之间不能再传递消息
+// 当正在接收的进程中没有消息时, 就会触发'disconnect'事件. 这经常在调用subprocess.disconnect()后立即触发
+// Note: 当子进程是一个Node.js, 可以在子进程内调用process.disconnect()方法来关闭IPC通道
+
+// subprocess.kill([signal])
+// signal <stirng>
+// subprocess.kill()方法向子进程发送一个信号. 如果没有给定参数, 则进程会发送'SIGTERM'信号
+// 如果信号没有被送达, ChildProcess对象可能会触发一个'error'事件
+
+// subprocess.killed
+// <boolean>: 当subprocess.kill()已成功发送信号给子进程后被设置为true
+// subprocess.killed属性表明该子进程是否已成功接收到subprocess.kill()的信号, 该属性不代表子进程是否已被终止
+
+// subprocess.pid
+// <number>: 整数, 返回子进程的进程标识(PID)
